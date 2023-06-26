63354a65d994edb72e3e2207ea59f9fc7e5cf498
gugkas <gugkas@yandex.ru>
2023-06-26 12:37:24 +0300

    change commit

--- km_in_minute.py
+++ km_in_minute.py
@@ -1,11 +1,12 @@
 
-
+=======
+>>>>>>> origin/branch1
 speed = float(input('введите скорость в км/час'))
 time = 60
 km_in_min = time / speed
 
-
+<<<<<<< HEAD
 print(km_in_min)
-
+=======
 print(km_in_min)
-
+>>>>>>> origin/branch1
